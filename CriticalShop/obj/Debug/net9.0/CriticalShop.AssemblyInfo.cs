ff632744fac5dc707a2813ab75e603cd38f0e585
Felipe Ferreira De Souza <felipefrs2007@gmail.com>
2025-09-01 13:16:21 -0300

    Adição inicial do estilo nas demais paginas do CRUD de produtos

--- CriticalShop/obj/Debug/net9.0/CriticalShop.AssemblyInfo.cs
+++ CriticalShop/obj/Debug/net9.0/CriticalShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CriticalShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963535ba7f3740457ea5dc7a4b1d7189b01af989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1b549c09071471e1643125ebacac3a656216df")]
 [assembly: System.Reflection.AssemblyProductAttribute("CriticalShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CriticalShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
